43c21dd64eddbbad8d640cb2820d60359d96070e
vasanth-selvaraj97 <vasanth.selvaraj@mavenberg.com>
2023-10-12 18:54:06 +0530

    social card changed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,7 +69,7 @@ const config = {
         },
       ],
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/logo.png",
       navbar: {
         title: "",
         logo: {
