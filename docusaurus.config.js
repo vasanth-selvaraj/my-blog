9eaa278fc86d488124bcab55f381aa8208e79945
vasanth-selvaraj97 <vasanth.selvaraj@mavenberg.com>
2023-10-12 11:30:20 +0530

    Added some SEO

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,121 +1,93 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Blog',
-  tagline: 'Thoughts of Vasanth',
-  favicon: 'img/favicon.ico',
+  title: "My Blog",
+  tagline: "Thoughts of Vasanth",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://vasanth-selvaraj.github.io',
+  url: "https://vasanth-selvaraj.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-blog/',
+  baseUrl: "/my-blog/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'vasanth-selvaraj', // Usually your GitHub org/user name.
-  projectName: 'my-blog', // Usually your repo name.
-  deploymentBranch:"gh-pages",
+  organizationName: "vasanth-selvaraj", // Usually your GitHub org/user name.
+  projectName: "my-blog", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: false,
         blog: {
           showReadingTime: true,
-          routeBasePath: '/',
+          routeBasePath: "/",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
+      
     ],
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      metadata: [{ name: "keywords", content: "tech, blog" }],
+      headTags: [
+        // Declare a <link> preconnect tag
+        {
+          tagName: "link",
+          attributes: {
+            rel: "preconnect",
+            href: "https://vasanth-selvaraj.github.io/vasanth/",
+          },
+        },
+      ],
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: '',
+        title: "",
         logo: {
-          alt: 'My Logo',
-          src: 'img/logo.png',
+          alt: "My Logo",
+          src: "img/logo.png",
         },
         items: [
           {
-            href: 'https://vasanth-selvaraj.github.io/vasanth/',
-            label: 'Portfolio',
-            position: 'right',
+            href: "https://vasanth-selvaraj.github.io/vasanth/",
+            label: "Portfolio",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'light',
-        links: [
-          // {
-          //   title: 'Docs',
-          //   items: [
-          //     {
-          //       label: 'Tutorial',
-          //       to: '/docs/intro',
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: 'Community',
-          //   items: [
-          //     {
-          //       label: 'Stack Overflow',
-          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-          //     },
-          //     {
-          //       label: 'Discord',
-          //       href: 'https://discordapp.com/invite/docusaurus',
-          //     },
-          //     {
-          //       label: 'Twitter',
-          //       href: 'https://twitter.com/docusaurus',
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: 'More',
-          //   items: [
-          //     {
-          //       label: 'Blog',
-          //       to: '/blog',
-          //     },
-          //     {
-          //       label: 'GitHub',
-          //       href: 'https://github.com/facebook/docusaurus',
-          //     },
-          //   ],
-          // },
-        ],
-        copyright: `Made with 💜 by Vasanth © ${new Date().getFullYear()}.`,
+        style: "light",
+        links: [],
+        copyright: `Made with 💜 by and © Vasanth ${new Date().getFullYear()}.`,
       },
       prism: {
         theme: lightCodeTheme,
